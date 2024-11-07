7ae8df06571d13a1978aa3afcb8714ff9f7073e4
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-11-07 23:36:26 +0600

    for deployment

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
-const corsOptions = {
-    origin: ['http://localhost:5173']
-}
+app.use(cors({origin: '*'}))
 
 
 
 //middleware
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n2g3mj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -34,7 +32,7 @@ const upload = multer({ storage });
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const clientCollection = client.db('seoPage1').collection('clients')
 
@@ -98,8 +96,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         //   await client.close();
